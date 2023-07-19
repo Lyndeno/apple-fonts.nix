a16ef93f168af7d4d19164ee778b7edc873a6a94
Lyndon Sanche <lsanche@lyndeno.ca>
2023-07-19 12:59:35 -0600

    Additional NoCC

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
         mkdir -p $out/share/fonts/truetype
       '';
       commonBuildInputs = with pkgs; [ undmg p7zip ];
-      makeAppleFont = (name: pkgName: src: pkgs.stdenv.mkDerivation {
+      makeAppleFont = (name: pkgName: src: pkgs.stdenvNoCC.mkDerivation {
         inherit name src;
 
         unpackPhase = unpackPhase pkgName;
