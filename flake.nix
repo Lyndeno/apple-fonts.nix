de6c044e84451ac5435aa4ec0e350e8ed7a03ed1
Lyndon Sanche <lsanche@lyndeno.ca>
2026-06-27 13:11:12 -0600

    feat: Use callPackage pattern

--- flake.nix
+++ flake.nix
@@ -61,65 +61,9 @@
         let
           pkgs = inputs.nixpkgs.legacyPackages.${system};
 
-          unpackPhase = pkgName: ''
-            runHook preUnpack
-            7z x $src
-            7z x './*/${pkgName}'
-            7z x 'Payload~'
-            runHook postUnpack
-          '';
-
-          commonInstall = ''
-            mkdir -p "$out/share/fonts/opentype"
-            mkdir -p "$out/share/fonts/truetype"
-          '';
-
-          commonHydraProducts = ''
-            mkdir -p "$out/nix-support"
-            for f in "$out/share/fonts/opentype/"* "$out/share/fonts/truetype/"*; do
-              [ -f "$f" ] && echo "file font $f" >> "$out/nix-support/hydra-build-products"
-            done
-          '';
-
-          makeAppleFont =
-            name: pkgName: src: nerd:
-            pkgs.stdenvNoCC.mkDerivation {
-              inherit name src;
-
-              unpackPhase = unpackPhase pkgName;
-
-              buildInputs =
-                [ pkgs.p7zip ]
-                ++ pkgs.lib.optionals nerd [
-                  pkgs.parallel
-                  pkgs.nerd-font-patcher
-                ];
-
-              setSourceRoot = "sourceRoot=`pwd`";
-
-              buildPhase = pkgs.lib.optionalString nerd ''
-                runHook preBuild
-                find \( -name \*.ttf -o -name \*.otf \) -print0 | parallel --will-cite -j $NIX_BUILD_CORES -0 nerd-font-patcher --no-progressbars -c {}
-                runHook postBuild
-              '';
-
-              installPhase =
-                ''
-                  runHook preInstall
-                ''
-                + commonInstall
-                + (
-                  if nerd then ''
-                    find -name \*.otf -maxdepth 1 -exec mv {} "$out/share/fonts/opentype/" \;
-                    find -name \*.ttf -maxdepth 1 -exec mv {} "$out/share/fonts/truetype/" \;
-                  '' else ''
-                    find -name \*.otf -exec mv {} "$out/share/fonts/opentype/" \;
-                    find -name \*.ttf -exec mv {} "$out/share/fonts/truetype/" \;
-                  ''
-                )
-                + commonHydraProducts
-                + ''runHook postInstall'';
-            };
+          #makeAppleFont =
+          #  name: pkgName: src: nerd:
+          #  ;
 
           fontDefs = [
             { name = "sf-pro";      pkgName = "SF Pro Fonts.pkg";      input = inputs.sf-pro; }
@@ -136,8 +80,8 @@
           acc: f:
           acc
           // {
-            ${f.name} = makeAppleFont f.name f.pkgName f.input false;
-            "${f.name}-nerd" = makeAppleFont "${f.name}-nerd" f.pkgName f.input true;
+            ${f.name} = pkgs.callPackage ./fontPackage.nix { inherit (f) name pkgName; src = f.input; nerd = false; };
+            "${f.name}-nerd" = pkgs.callPackage ./fontPackage.nix { inherit (f) pkgName; name = "${f.name}-nerd"; src = f.input; nerd = true; };
           }
         ) { } fontDefs
         // {
