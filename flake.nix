ca00c9076a9a8d36e3a98ad7e4c345d71440d7c4
Lyndon Sanche <lsanche@lyndeno.ca>
2026-08-21 22:10:43 -0600

    feat: Support (untested) all nixpkgs platforms

--- flake.nix
+++ flake.nix
@@ -43,12 +43,7 @@
     ci,
     ...
   }: let
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+    systems = builtins.attrNames inputs.nixpkgs.legacyPackages;
 
     forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
 
